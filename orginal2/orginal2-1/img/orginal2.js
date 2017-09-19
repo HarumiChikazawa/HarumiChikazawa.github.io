549cc85241c8e527ba6e70a514cd7419eb2af97c
HarumiChikazawa <30514996+HarumiChikazawa@users.noreply.github.com>
2017-09-19 17:51:42 +0900

    orginal2
    free bird

---
+++ orginal2/orginal2-1/img/orginal2.js
@@ -0,0 +1,23 @@
+$("#imagephoto").skippr({
+
+	transition : 'fade',
+
+	speed : 1000,
+
+	easing : 'easeOutQuart',
+	
+	navtype : 'bubble',
+
+	childrenElementType : 'div',
+	
+	arrows : false,
+	
+	autoPlay : true,
+	
+	autoPlayDuration : 2000,
+	
+	keyboardOnAlways : true,
+	
+	hidePrevious : false,
+
+});
